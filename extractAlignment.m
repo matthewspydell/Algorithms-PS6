62fc8768866f9dc0c55a6ebbb7732336d81afb2e
cseamans3 <case0253@colorado.edu>
2017-03-21 10:30:12 -0600

    Update extractAlignment.m

--- extractAlignment.m
+++ extractAlignment.m
@@ -6,10 +6,11 @@
 
 function a = extractAlignment(S,x,y)
     a = [];
-    i = length(x);
-    j = length(y);
+    i = length(x)+1;
+    j = length(y)+1;
     
     while (i>0 || j>0)
-        
+        a[i] = determineOptimalOp(S, i, j, x, y);
+        [i, j] = updateIndices(S, i, j, a);
     end
 end
