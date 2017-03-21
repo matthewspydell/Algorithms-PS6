fd09fdb1c2952fff3d4f13809c79c1792a2d32ef
cseamans3 <case0253@colorado.edu>
2017-03-21 12:08:24 -0600

    Update commonSubstrings.m

--- commonSubstrings.m
+++ commonSubstrings.m
@@ -1,7 +1,20 @@
-function substring = commonSubstrings(x, L, a)
-  i = 1;
-  while (a(i) ~= '|')
-    
+function substrings = commonSubstrings(x, L, a)
+  substrings = [];
+  temp = [];
+  L_count = 0;
+  
+  %Increment through x and a
+  for i = 1 to length(a)
+      if (a(i) == '|')
+        L_count = L_count + 1;
+        temp = [temp x(i)];
+        if (L_count >= L) %Add common substring to returning array
+           substrings = [substrings; temp];
+        end
+      else
+          L_count = 0;
+          temp = [];
+      end
   end
-
+  
 end
