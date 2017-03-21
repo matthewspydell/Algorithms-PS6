2e7fdf4e2e8bc6fa3d314e227aa42fcaa5fd5eb9
matthewspydell <matthew.spydell@colorado.edu>
2017-03-21 17:17:49 -0600

    reset

--- extractAlignment.m
+++ extractAlignment.m
@@ -5,11 +5,27 @@
 %
 
 function a = extractAlignment(S,x,y)
-    a = [];
-    i = length(x);
-    j = length(y);
+    a = char([]);
+    i = length(x)+1;
+    j = length(y)+1;
+    disp('Trace back coordinates')
+    fprintf('i = %d    j = %d\n', i, j);
     
-    while (i>0 || j>0)
-        
+    while (i>1 && j>1)
+        [a(end+1), i, j] = determineOptimalOp(S, i, j, x, y);
+        fprintf('i = %d    j = %d\n', i, j);
+    disp('finished while loop')
+    if (i>0)
+        for k = 1:i-1
+            fprintf('i = %d    j = %d\n', i-k, j);
+            a(end+1) = 'i';
+        end
     end
+    if (j>0)
+        for k = 1:j-1
+            fprintf('i = %d    j = %d\n', 0, j-k);
+            a(end+1) = 'd';
+        end
+    end
+    a = fliplr(a);
 end
