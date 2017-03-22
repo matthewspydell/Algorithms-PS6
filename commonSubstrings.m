b1a26d08392b31cb36ba5b001e44562036ac2179
cseamans3 <case0253@colorado.edu>
2017-03-22 17:46:48 -0600

    Update commonSubstrings.m
    Added comments

--- commonSubstrings.m
+++ commonSubstrings.m
@@ -6,27 +6,27 @@ function substrings = commonSubstrings(x, L, a)
   
   %Index through a
   for i = 1:length(a)
-      if (a(i) == 'i')
+      if (a(i) == 'i') %Add in spaces to x if an insertion operation took place
           if (i == 1)
              x = ['_' x]; 
           else
              x = [x(1:i-1) '_' x(i:length(x))];
           end
       else
-        if (a(i) == '|')
-            L_count = L_count + 1;
-            temp = [temp x(i)];
+        if (a(i) == '|') %If there is a no-op character:
+            L_count = L_count + 1; %Increment no-op counter
+            temp = [temp x(i)]; %Add the corresponding character to a temp storage
             if (L_count >= L) %Add common substring to returning array
-                indicator = 1;
-                if (i == length(a))
+                indicator = 1; %To indicate that a sufficiently long substring has been found.
+                if (i == length(a)) %If a substring is found at the end of the array
                    substrings = [substrings ' ' temp]; 
                 end
             end
       else
-          if (indicator)
-             substrings = [substrings ' ' temp]; 
+          if (indicator) %If substring of length at least L has been found
+             substrings = [substrings ' ' temp]; %Add the substring to the returning array
           end
-          indicator = 0;
+          indicator = 0; %Reset variables and temporary substring holder
           L_count = 0;
           temp = [];
       end
