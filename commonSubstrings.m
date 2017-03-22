9023b25da887d6e1ed15851a6a61f7b5b9a3e398
cseamans3 <case0253@colorado.edu>
2017-03-21 21:30:43 -0600

    Update commonSubstrings.m
    Added comments

--- commonSubstrings.m
+++ commonSubstrings.m
@@ -4,20 +4,20 @@ function substrings = commonSubstrings(x, L, a)
   indicator = 0;
   L_count = 0;
   
-  %Increment through x and a
+  %Increment through a, looking for a no-op character
   for i = 1:length(a)
       if (a(i) == '|')
-        L_count = L_count + 1;
-        temp = [temp x(i)];
-        if (L_count >= L) %Add common substring to returning array
+        L_count = L_count + 1; %Increment the count of no-ops
+        temp = [temp x(i)]; %Concatenate the letter of x that corresponds to the no-op found
+        if (L_count >= L) %If the substring is at least as long as L, set indicator
            indicator = 1;
         end
       else
-          if (indicator)
-             substrings = [substrings ' ' temp]; 
+          if (indicator) %If there was a substring at least as long as L
+             substrings = [substrings ' ' temp]; %Add previously found common substring to returning array
           end
-          L_count = 0;
-          temp = [];
+          L_count = 0; 
+          temp = []; %Reset the substring holder
       end
   end
   
