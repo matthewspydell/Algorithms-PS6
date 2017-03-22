d78939438b4c11d4f64c39d918c161274d966374
matthewspydell <matthew.spydell@colorado.edu>
2017-03-22 15:12:49 -0600

    I think commonSubstrings is working

--- commonSubstrings.m
+++ commonSubstrings.m
@@ -4,24 +4,69 @@
 % length L or greater are found
 
 function substrings = commonSubstrings(x, L, a)
-
-    substrings = char(zeros(length(x)));
-    index = char([]);
-    
-    for i = 1:length(x)
-        if (a(i) == '|')
-            count = count +1;
-            index(end+1) = x(i-1);
-            if count >= L
-                for j = 1:length(index)
-                    
-                end
-            end
-        else
-            count = 0;
-            index = [];
-        end
-
+  substrings = char([]);
+  temp = char([]);
+  indicator = false;
+  L_count = 0;
+  j = 1;
+  i = 1;
+  k = 1;
+  
+  % while the index of the string is equal to one iterate through the
+  % operations in a. This ensures that the blanks that are inserted before
+  % each string do not have an affect on the commonString function
+  while (i == 1)
+    if (a(k) == 'i' || a(k) == '|' || a(k) == 's')
+        i = i+1;    % if operation moves path one row down exit
+    elseif (a(k) == 't')
+        i = i+1;    % if operation moves path two rows down increment starting string index by one and exit
+        j = j+1;
     end
-
+    k = k+1;    % move to next operation in a
+  end
+  
+  %Index through a starting at first valid operation
+  for i = k:length(a)
+      fprintf('i = %d    j = %d\n', i, j);
+      
+      if (a(i) == '|')  % if noop, add letter to temp substring and move index of string ahead by one
+        L_count = L_count + 1;  % increment the length of the current substring
+        temp = [temp, x(j)];  % concatenate the letter of x that corresponds to the no-op onto the current substring
+        if (L_count == L)   % if the substring is at least as long as L, set indicator
+           indicator = true;   % so that once a non noop operation is hit temp substring is added to substrings
+        end
+        j = j+1;    % move to next letter
+        
+      elseif (a(i) == 'd')   % if indel from left do not move to next index in string
+        if (indicator) % if there was a substring at least as long as L
+           substrings = [substrings, ', ', temp]; % add previously found common substring to returning array
+           indicator = false;
+        end 
+        L_count = 0; 
+        temp = []; % reset the substring holder
+          
+      elseif (a(i) == 't') % if swap move index of string ahead by two
+        if (indicator) % if there was a substring at least as long as L
+           substrings = [substrings, ' ', temp]; % add previously found common substring to returning array
+           indicator = false;
+        end   
+        L_count = 0; 
+        temp = []; % reset the substring holder
+        j = j+2;    % move to the next second letter
+          
+      else  % if sub move index of string ahead by one
+        if (indicator) % if there was a substring at least as long as L
+           substrings = [substrings, ' ', temp]; % add previously found common substring to returning array
+           indicator = false;
+        end    
+        L_count = 0; 
+        temp = []; % reset the substring holder
+        j = j+1;    % move to next letter
+      end
+  end
+  
+  if (indicator) % if last operation of string was a noop check if substring needs to be added
+           substrings = [substrings, ' ', temp]; % add previously found common substring to returning array
+  end
+  
 end
